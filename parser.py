1c9d90f6770a15d5cc7b045fb8adfa4ff52ab9c6
HashirSalam <hashersalam@gmail.com>
2019-08-17 14:18:49 +0500

    Major Refractoring

--- parser.py
+++ parser.py
@@ -16,7 +16,7 @@ def scrape_and_grab(link):
 
   try:
     resp = requests.get(link, headers={'User-Agent': user_agent}, timeout=20, verify=True)
-    headers = ["h1", "h2", "h3", "h4", "h5", "h6"]
+    headers = ["h1", "h2", "h3"]
     soup = bs(resp.text, 'lxml')
 
 
@@ -24,14 +24,19 @@ def scrape_and_grab(link):
     #relatedTerms = []
     for header in soup.find_all(headers):
         headerText = header.get_text().strip()
-        # print("Header Tag content :")
-        # print(headerText)
-        little_dict = {"HEADING": headerText}
+        #check for matching words in heading
+        found_words = [word for word in Matching if word in headerText]
+        if (found_words):
+            little_dict = {"HEADING": headerText}
+        else: 
+            #Do nothing for that heading where no matching word found
+            continue
+        
        
         for elem in header.next_siblings:
             if elem.name and elem.name.startswith('h'): # case if next element is header too
             
-                break  
+                break  #becaus we are dropping that header 
             if elem.name == 'p': #case if next element is paragraph
                 paragraphText = elem.get_text().strip()
                 #check for matching words in paragraph
@@ -50,91 +55,76 @@ def scrape_and_grab(link):
     print ("ERROR : " + link)
     #print (e)
 
-def findTopic(word,Heading):
+def findTopic(word,Heading,paragraph):
     #print("Word  :"+ word +" Heading "+ Heading)
     if word in Heading:
         #print("Found :"+ word +" in "+ Heading)
        
-        return(word,Heading)
+        return(word,Heading,paragraph)
 
 
 def createTopic(DF,Term,combos):
-    #print("DF :",DF)
+    # print("DF :",DF)
     # print("Term :",Term)
-    #print("combos :",combos)
-    texd = []
-    for index, row in DF.iterrows(): # for each row (page)
-        #print(row["Page"])
-        #print([d['HEADING'] for d in row["Page"]])
-        texd.append([d['HEADING'] for d in row["Page"]])
-    HeadingsPerTerm = [i for sublist in texd for i in sublist]
-    
-   
-
-    dfObj = pd.DataFrame(columns=['Related Term','Heading','Topic'])   
-    
-    for Heading in HeadingsPerTerm:
+    print("combos :",combos)
+    dfObj = pd.DataFrame(columns=['Related Term','Heading','Paragraph','Topic'])
+    for index, row in DF.iterrows():
         for word in combos:
-            result = findTopic(word.lower(),Heading.lower())
-            # tmp = wordnet.synsets(word)[0].pos()
-            # print(word, ":", tmp)
-            # if (tmp == 'n'):
+            result = findTopic(word.lower(),row['Heading'].lower(),row['Paragraph'])
             wordGroup = inflection.singularize(word)+","+inflection.pluralize(word)
-            # else:
-            #     wordGroup = word
             if result is not None:
-                word,Heading = result
-                dfObj = dfObj.append({'Related Term':Term,'Heading': Heading,'Topic':wordGroup}, ignore_index=True)
-   
-    dfObj.drop_duplicates(subset=['Heading', 'Topic'],inplace=True) # remove duplicates
-    
-    dfObj = dfObj.groupby('Heading').agg({'Related Term':'first','Topic': ', '.join}).reset_index()  #Group by Heading
-           
-    dfObj = dfObj[['Related Term','Heading','Topic']] #Re-arranging again
-    print(dfObj)
-    
+                word,Heading,paragraph = result
+                dfObj = dfObj.append({'Related Term':Term,'Heading': Heading,'Paragraph': paragraph,'Topic':wordGroup}, ignore_index=True)
+
+    dfObj = dfObj.groupby('Heading').agg({'Related Term':'first','Paragraph':'first','Topic': ', '.join}).reset_index()  #Group by Heading   
+    dfObj = dfObj[['Related Term','Heading','Paragraph','Topic']] #Re-arranging again
+    #print (dfObj.sort_values(by="Topic", ascending=False))
+    return (dfObj.sort_values(by="Topic", ascending=False))
+
           
 
 
-def combifyData(DF):
-   
+def combifyData(DF): 
     grouped_df = DF.groupby('Related Terms')
-    DF.insert(3, "Topic","")# used in next function
-    dataframes = []
+    dataframes = pd.DataFrame(columns=['Related Term','Heading','Paragraph','Topic'])  
     #print(DF)
     for key, item in grouped_df:
         relatedTerm = key
         texd = []
         #print(relatedTerm, "\n\n")
-        for pages in item["Page"]:
+        for heading in item["Heading"]:
             #print([d['PARAGRAPH'] for d in pages])
-            texd.append([d['PARAGRAPH'] for d in pages])
-        parasPerTerm = [i for sublist in texd for i in sublist]
+            texd.append(heading)
+        #textPerTerm = [i for sublist in texd for i in sublist]
         
-        text = ' '.join(parasPerTerm)
-        combos = combify(text, 1, stop_words=stopwords, limit=2)
-        #average = combos["NUMBER_OF_TIMES_FOUND"].mean() # calculate average
-        #average = average + 1 # Add 1 to average
-        df = combos[combos["NUMBER_OF_TIMES_FOUND"] >= 2] # Filter value less than average
+        text = ' '.join(texd)
+        #print(text)
+        combos = combify(text, 1, stop_words=stopwords)
+        average = combos["NUMBER_OF_TIMES_FOUND"].mean() # calculate average
+        average = average + 1 # Add 1 to average
+        df = combos[combos["NUMBER_OF_TIMES_FOUND"] > average] # Filter value based on criteria
         df= df.sort_values(by="NUMBER_OF_TIMES_FOUND", ascending=False) #Sort in Desc
 
         orderedCombos = tuple(list(df.index))
-        createTopic(grouped_df.get_group(key),relatedTerm,orderedCombos)
-        # dataframes.append(df)
-        #print(orderedCombos)
-    # with open('output.txt', 'w') as filehandle:
-    #     for data in dataframes:
-    #         filehandle.write('%s\n' % data)  
+        dfObj = createTopic(grouped_df.get_group(key),relatedTerm,orderedCombos) # function call for each (DF for that related term , related term , and particular combo)
+        #print (dfObj)
+        frames = [dataframes,dfObj]
+        dataframes = pd.concat(frames)
+    # path='Output\\'
+    # dfObj.to_csv(path+relatedTerm+'.csv',index=False, encoding='utf-8')
+    dataframes.to_csv("Output.csv",index=False, encoding='utf-8')
+    #print(dataframes)
+   
         
       
  
 
 #Read CSV for URLs
-df = pd.read_csv("Full.csv",index_col=False) 
+df = pd.read_csv("Full2.csv",index_col=False) 
 #store relavant data
 df=df[['URL','Related Terms']]
 
-matchFrame = pd.read_csv("Plumbing Entities.csv",index_col=False)
+matchFrame = pd.read_csv("Plumbing Entities2.csv",index_col=False)
 #get matching words in list
 matchFrame = matchFrame[['matched_text']]
 Matching = matchFrame['matched_text'].tolist()
@@ -149,7 +139,23 @@ for index, row in df.iterrows():
         if(len(page)!=0):
             dfObj = dfObj.append({'URL': link,'Related Terms':relatedTerms, 'Page': page}, ignore_index=True)
 
-#######################################################################################################################
+##################################################### HISTORY ##################################################################
+
+History = pd.DataFrame(columns=['URL','Related Terms','Heading','Paragraph']) 
+for index, row in dfObj.iterrows(): # for each row (page)
+    term = row["Related Terms"]
+    url = row["URL"]
+    for page in row["Page"]:
+        heading = page.get('HEADING')
+        paragraph = page.get('PARAGRAPH')
+        History = History.append({'URL': url,'Related Terms':term, 'Heading': heading,'Paragraph':paragraph}, ignore_index=True) 
+
+History.drop_duplicates(inplace=True)
+#print(History)
+##########################################################################################################################
+
+combifyData(History)
 
 
-combifyData(dfObj)
+
+
